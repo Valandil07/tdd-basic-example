b263db26a446402360b8f942aa9f54cff3834c14
markus-posch <markus.posch1@stud-fh.campuswien.ac.at>
2023-05-11 20:24:08 +0200

    added divide and multiply to calculator

--- src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Calculator.java
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Calculator.java
@@ -21,14 +21,27 @@ public class Calculator {
     }
 
     public int divide(int dividend, int divisor) {
-        throw new UnsupportedOperationException();
+        if(divisor==0){
+            throw new ArithmeticException("Division with 0 not allowed");
+        }
+        else {
+            return (dividend / divisor);
+        }
     }
 
     public int multiply(int firstOperand, int secondOperand) {
-        throw new UnsupportedOperationException();
+        if(firstOperand == 0){
+            return 0;
+        } else if (secondOperand == 0) {
+            return 0;
+        } else {
+            return (firstOperand * secondOperand);
+        }
     }
 
     public int factorial(int firstOperand) {
         throw new UnsupportedOperationException();
     }
+
+
 }
