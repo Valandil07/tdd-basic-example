f4928bef394239d6ff475ec90b10755c15be96fc
Bernhard Vogler <bernhardv87@gmail.com>
2023-05-13 15:25:13 +0200

    Unittest for US2 (Adding Tags), implemented local threshold

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarkTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BookmarkTest {
@@ -16,5 +18,25 @@ public class BookmarkTest {
     @Test
     public void invalidURLThrowsException(){
         assertThrows(MalformedURLException.class, () -> new Bookmark(new URL("xxx")));
+    }
+
+    @Test
+    public void addTagToBookmark(){
+        Bookmark bookmark = new Bookmark(new URL("https://www.orf.at/"));
+
+        bookmark.addTag("News");
+    }
+
+    @Test
+    public void checkIfTwoTagsAreAddedToBookmark(){
+        int expected = 2;
+        Bookmark bookmark = new Bookmark(new URL("https://www.orf.at/"));
+        bookmark.addTag("News");
+        bookmark.addTag("Sports");
+
+        ArrayList<String> taglist = bookmark.getTags();
+        int result = taglist.size();
+
+        assertEquals(expected, result);
     }
 }
